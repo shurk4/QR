c242f0547c724c3ade48d341eb8417544a5b9ea1
shurk4 <59886241+shurk4@users.noreply.github.com>
2023-08-13 00:40:58 +0300

    txtfiles help added

--- help.h
+++ help.h
@@ -3,6 +3,11 @@
 
 #include <QDialog>
 
+enum HelpType
+{
+    TXTFILES
+};
+
 namespace Ui {
 class help;
 }
@@ -12,8 +17,10 @@ class help : public QDialog
     Q_OBJECT
 
 public:
-    explicit help(QWidget *parent = nullptr);
+    help(QWidget *parent = nullptr);
     ~help();
+
+    void showTextfilesHelp();
 
 private slots:
     void on_pushButton_clicked();
